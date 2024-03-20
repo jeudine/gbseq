6dccb747e24f478bddd43e38a33d1c651a349e2f
julien <julien@eudine.fr>
2024-03-20 22:46:00 +0100

    Transition from break to drop done

--- src/mbreak.rs
+++ src/mbreak.rs
@@ -1,8 +1,8 @@
 use crate::hh::HH;
 use midir::MidiOutputConnection;
 use rand::rngs::ThreadRng;
-use tseq::sequence::{end_note, param_value, start_note, Sequence, LFO, SP2, SP3};
-use tseq::{log_send, Transition};
+use tseq::sequence::{control_change, param_value, start_note, Sequence, CC_SP1_LAYER, SP1};
+use tseq::{log_send, Stage, Transition};
 
 #[derive(Copy, Clone, Default)]
 pub struct Break0 {
@@ -21,6 +21,20 @@ impl Sequence for Break0 {
 		root: u8,
 		transition: Transition,
 	) {
+		let t = step % 96;
+
+		if let Transition::Out(Stage::Drop) = transition {
+			if t == 0 {
+				log_send(conn, &start_note(channel_id, SP1, param_value(0.6)));
+			} else if t == 24 {
+				log_send(conn, &start_note(channel_id, SP1, param_value(0.5)));
+			} else if t == 48 {
+				log_send(conn, &start_note(channel_id, SP1, param_value(0.4)));
+			} else if t == 84 {
+				log_send(conn, &control_change(channel_id, CC_SP1_LAYER, 1 << 6));
+				log_send(conn, &start_note(channel_id, SP1, param_value(0.0)));
+			}
+		}
 		if oh {
 			self.hh.trigger_oh(step, conn, root, rng);
 		}
