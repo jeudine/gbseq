a62672579c02fb5b9f5f74356d95077928b3bf08
julien <julien@eudine.fr>
2024-03-26 23:24:58 +0100

    random freeze done

--- src/breakbeat.rs
+++ src/breakbeat.rs
@@ -3,14 +3,15 @@ use midir::MidiOutputConnection;
 use rand::rngs::ThreadRng;
 use rand::Rng;
 use tseq::sequence::{
-	control_change, end_note, param_value, start_note, Sequence, CC_SP1_LAYER, CC_SP1_LENGTH,
-	CC_SP2_LAYER, CC_SP2_LENGTH, CC_SP3_LAYER, CC_SP3_LENGTH, CC_SP4_LAYER, CC_SP4_LENGTH, LFO,
-	SP1, SP2, SP3, SP4,
+	cc_parameter, control_change, end_note, param_value, start_note, Sequence, CC_FREEZE,
+	CC_SP1_LAYER, CC_SP1_LENGTH, CC_SP2_LAYER, CC_SP2_LENGTH, CC_SP3_LAYER, CC_SP3_LENGTH,
+	CC_SP4_LAYER, CC_SP4_LENGTH, LFO, SP1, SP2, SP3, SP4,
 };
 use tseq::Stage;
 use tseq::{log_send, Transition};
 
 const TRIG_PROBA: f64 = 0.7;
+const FREEZE_PROBA: f64 = 0.7;
 
 const SP_ARRAY: [u8; 3] = [SP2, SP3, SP4];
 const LAYER_ARRAY: [u8; 3] = [0x00, 0x40, 0x60];
@@ -26,6 +27,7 @@ struct Trig {
 pub struct Breakbeat0 {
 	hh: HH,
 	trigs: [Option<Trig>; 16],
+	frozen: Option<(u8, u32)>, // (sp, step unfreeze)
 }
 
 // TODO: try adding LFO on some effects
@@ -60,12 +62,32 @@ impl Sequence for Breakbeat0 {
 			log_send(conn, &control_change(channel_id, CC_SP3_LENGTH, 31));
 		}
 
-		//if t == 0 || t == 36 {
-		if t == 0 || t == 24 || t == 48 || t == 72 {
+		if t == 0 || t == 36 {
 			log_send(conn, &start_note(channel_id, SP1, param_value(0.0)));
 		}
 
 		if t % 6 == 0 {
+			if let Some(f) = self.frozen {
+				if f.1 >= step {
+					log_send(
+						conn,
+						&control_change(channel_id, cc_parameter(CC_FREEZE, f.0), 63),
+					);
+				}
+			}
+
+			if rng.gen_bool(FREEZE_PROBA) {
+				let sp = rng.gen_range(0..3);
+				let val = rng.gen_range(0..=127);
+				let t = rng.gen_range(1..=3);
+
+				self.frozen = Some((sp, step + t * 24));
+				log_send(
+					conn,
+					&control_change(channel_id, cc_parameter(CC_FREEZE, sp), val),
+				);
+			}
+
 			let i = t / 6;
 			if let Some(t) = self.trigs[i as usize] {
 				log_send(
